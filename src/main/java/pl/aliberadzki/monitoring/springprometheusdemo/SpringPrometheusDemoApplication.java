27e653f22014212794fb88001db108631570050e
aliberadzki <adam.liberadzki@gmail.com>
2018-12-10 17:05:11 +0100

    spring boot initialized

---
+++ src/main/java/pl/aliberadzki/monitoring/springprometheusdemo/SpringPrometheusDemoApplication.java
@@ -0,0 +1,12 @@
+package pl.aliberadzki.monitoring.springprometheusdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPrometheusDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPrometheusDemoApplication.class, args);
+    }
+}
